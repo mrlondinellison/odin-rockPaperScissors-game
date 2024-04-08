2a83a2c83a1a45b2741aac4644854c2cb55cc1ab
Lunden Ellison <lunden.e@icloud.com>
2024-04-08 16:54:04 -0400

    add playRound() function that simulates a round of rock paper scissors

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function playRound(playerSelection,cpuSelection) {
+    playerSelection = prompt('Rock, Paper or Scissors?: ', '').toLowerCase();
+    cpuSelection = getComputerChoice();
+    function uppercase(word) {
+        return word.replace(word.charAt(0), word.charAt(0).toUpperCase())
+    }
+    
+    if ((playerSelection === 'rock' && cpuSelection === 'scissors') || (playerSelection === 'paper' && cpuSelection === 'rock') || (playerSelection === 'scissors' && cpuSelection === 'paper')) {
+        return `You Win! ${uppercase(playerSelection)} Beats ${uppercase(cpuSelection)}.`;
+    } else if ((playerSelection === 'scissors' && cpuSelection === 'rock') || (playerSelection === 'rock' && cpuSelection === 'paper') || (playerSelection === 'paper' && cpuSelection === 'scissors')) {
+        return `You Lose! ${uppercase(playerSelection)} Cant Beat ${uppercase(cpuSelection)}.`;
+    } else if (playerSelection === cpuSelection) {
+        return 'Its a Tie!';
+    } else return 'Invalid Entry! Please Try Again.';
+}  
+
+console.log(playRound());
